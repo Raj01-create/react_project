eb9ff2d245d36fe3c9f83224baac8dd2671db1e7
Raj01-create <rajendrakeshari.pal014183@rec.ac.in>
2026-06-12 23:02:30 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Test1() {
+  return (
+    <div>
+      <h3>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, dignissimos!
+
+      </h3>
+    </div>
+  )
+}
+
+export default Test1
+
+export function Test(){
+  return(
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur porro omnis consequatur doloremque tenetur laborum iste dolorum cupiditate explicabo beatae aliquam aperiam ea reiciendis, ad velit, illo doloribus. Dolorum, consequatur.</p>
+  )
+}
